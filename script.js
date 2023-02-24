c7d0f36c08e5b423c1681849c8887781ab8b2fc6
saulwade <saulwade29@gmail.com>
2023-02-24 13:57:28 -0600

    added the rest of the code for the js part

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function writePassword() {
+  var upperLetters = ["A", "E", "I", "O", "U"];
+  var lowerLetters = ["a", "e", "i", "o", "u"];
+  var specialCharacters = ["*", "#", "!"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  var lenght = 8;
+  var pass = "";
+
+  for (var i = 0; i <= lenght; i++){
+    var randomnumb = Math.floor(Math.random() * specialCharacters.numbers.lowerLetters.upperLetters.lenght);
+  }
+
+  document.getElementById('password').value = password;
+
+
+}
 
 
